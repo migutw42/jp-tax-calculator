1030f20315b0f661993c7aca33bb3f6fb9753503
東海林　昴 <s-shoji@bnba.jp>
2017-08-28 15:18:20 +0900

    Delete state and add form.

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -6,48 +6,49 @@ import Button from 'material-ui/Button';
 import TaxCalculator from '../libs/TaxCalculator';
 
 class TextFields extends Component {
-  state = {
-    income: 0,
-    expenses: 0
-  };
+  handleSubmit(event) {
+    event.preventDefault();
 
-  calculate() {
-    const { income, expenses } = this.state;
-    const taxInfo = TaxCalculator.getTaxInfo(income, expenses);
+    const formElement = event.target;
+    const taxInfo = TaxCalculator.getTaxInfo(
+      formElement.income.value,
+      formElement.expenses.value
+    );
     this.props.handleUpdate(taxInfo);
   }
 
   render() {
     return (
       <Card>
-        <CardContent>
-          <div>
-            <TextField
-              label="Income"
-              type="number"
-              value={this.state.income}
-              onChange={event => this.setState({ income: event.target.value })}
-              inputProps={{ min: 0, step: 10000 }}
-              margin="normal"
-              style={{ width: '100%' }}
-            />
-          </div>
-          <div>
-            <TextField
-              label="Expenses"
-              type="number"
-              value={this.state.expenses}
-              onChange={event =>
-                this.setState({ expenses: event.target.value })}
-              inputProps={{ min: 0, step: 10000 }}
-              margin="normal"
-              style={{ width: '100%' }}
-            />
-          </div>
-        </CardContent>
-        <CardActions>
-          <Button onClick={() => this.calculate()}>Calculate</Button>
-        </CardActions>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <CardContent>
+            <div>
+              <TextField
+                label={TaxCalculator.translate('income')}
+                name="income"
+                type="number"
+                defaultValue="0"
+                inputProps={{ min: 0, step: 10000 }}
+                margin="normal"
+                style={{ width: '100%' }}
+              />
+            </div>
+            <div>
+              <TextField
+                label={TaxCalculator.translate('expenses')}
+                name="expenses"
+                type="number"
+                defaultValue="0"
+                inputProps={{ min: 0, step: 10000 }}
+                margin="normal"
+                style={{ width: '100%' }}
+              />
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button type="submit">Calculate</Button>
+          </CardActions>
+        </form>
       </Card>
     );
   }
